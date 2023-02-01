24300a7fb5a4c8791e161a2f328cd53012570123
Stephy <atienostephanie040@gmail.com>
2023-02-01 12:52:16 +0000

    check if number is even or odd

---
+++ even.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# checking if a number is even or odd
+
+number=0
+
+read -p "Enter a number > " number
+
+echo "Number is $number"
+if [ $((number % 2)) -eq 0 ]; then
+  echo "Number is even"
+else
+  echo "Number is odd"
+fi
